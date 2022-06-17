a372ca62d325e4b01040a716e7cf7646d62c80c3
Mc-shelton <sheltonnito@gmail.com>
2022-06-17 17:33:18 +0300

    right

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -165,7 +165,7 @@ function Main(props) {
           <Route path="/watchVid" component={watchVid} />
           <Route path="/ProfileSet" component={ProfileSet} />
           <Redirect to={{
-            pathname:'/Search',
+            pathname:'/Dashboard',
             state:{toggleB:true,userData:Data}
 
           }} />
